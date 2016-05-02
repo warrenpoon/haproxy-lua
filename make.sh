68a94f7a5862719664c464c68d5a788fef9e4fa1
Michael Laccetti <mlaccetti@uncharted.software>
2016-05-02 12:11:54 -0400

    Tagging as 1, 1.6, and 1.6.4

--- make.sh
+++ make.sh
@@ -2,9 +2,12 @@
 
 docker build -t mlaccetti/haproxy-lua .
 docker build -t mlaccetti/haproxy-lua:1.6-alpine .
+docker build -t mlaccetti/haproxy-lua:1.6.4-alpine .
 
 docker push mlaccetti/haproxy-lua
 docker push mlaccetti/haproxy-lua:1.6-alpine
+docker push mlaccetti/haproxy-lua:1.6.4-alpine
 
 docker pull mlaccetti/haproxy-lua
 docker pull mlaccetti/haproxy-lua:1.6-alpine
+docker pull mlaccetti/haproxy-lua:1.6.4-alpine
